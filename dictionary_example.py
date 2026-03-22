face8622304e1fbcc148f917e5a3ab5dbf7c3acb
Varshika-devi <varshikadevi3@gmail.com>
2026-03-22 09:22:01 +0530

    Create dictionary_example.py

---
+++ dictionary_example.py
@@ -0,0 +1,9 @@
+student = {
+    "name": "Alice",
+    "age": 20,
+    "course": "Computer Science"
+}
+
+print("Student Name:", student["name"])
+print("Student Age:", student["age"])
+print("Student Course:", student["course"])
