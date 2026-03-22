207f349ed74d3c138023f448822fdd7d27963e0a
Varshika-devi <varshikadevi3@gmail.com>
2026-03-22 09:23:15 +0530

    Create list_operations.py

---
+++ list_operations.py
@@ -0,0 +1,9 @@
+fruits = ["apple", "banana", "cherry"]
+
+fruits.append("orange")
+print("After adding:", fruits)
+
+fruits.remove("banana")
+print("After removing:", fruits)
+
+print("First fruit:", fruits[0])
