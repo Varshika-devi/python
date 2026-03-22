478e00172d350be31d92ebe799204c2c53eaa4ae
Varshika-devi <varshikadevi3@gmail.com>
2026-03-22 09:28:30 +0530

    Create calculator_functions.py

---
+++ calculator_functions.py
@@ -0,0 +1,16 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    return a / b
+
+print("Add:", add(10, 5))
+print("Subtract:", subtract(10, 5))
+print("Multiply:", multiply(10, 5))
+print("Divide:", divide(10, 5))
