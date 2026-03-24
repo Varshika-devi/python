f09001cca0e8a816f31df01924200d558d420aa0
Varshika-devi <varshikadevi3@gmail.com>
2026-03-24 07:32:24 +0530

    Create simple_chatbot.py

---
+++ simple_chatbot.py
@@ -0,0 +1,14 @@
+print("Chatbot: Hello! Type 'bye' to exit.")
+
+while True:
+    user = input("You: ").lower()
+
+    if user == "hello":
+        print("Chatbot: Hi there!")
+    elif user == "how are you":
+        print("Chatbot: I'm fine!")
+    elif user == "bye":
+        print("Chatbot: Goodbye!")
+        break
+    else:
+        print("Chatbot: I don't understand.")
