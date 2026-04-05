cab72b03352a4eee8aae284cd2465c4830c4b6b1
Varshika-devi <varshikadevi3@gmail.com>
2026-04-05 08:19:52 +0530

    Create json_to_csv.py

---
+++ json_to_csv.py
@@ -0,0 +1,12 @@
+import json
+import csv
+
+with open("data.json") as f:
+    data = json.load(f)
+
+with open("data.csv", "w", newline="") as f:
+    writer = csv.DictWriter(f, fieldnames=data[0].keys())
+    writer.writeheader()
+    writer.writerows(data)
+
+print("Converted to CSV!")
